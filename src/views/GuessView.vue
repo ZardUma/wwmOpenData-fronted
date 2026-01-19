bdc18c82b9ad4d0775d1751c5b9158cdd64b04b5
Eric <sr56225214@gmail.com>
2026-01-19 22:25:06 +0800

    fix

---
+++ src/views/GuessView.vue
@@ -0,0 +1,29 @@
+﻿<script setup>
+import { onMounted, ref } from 'vue';
+import { get } from '@/api/http';
+
+const data = ref(null);
+const error = ref(null);
+
+onMounted(async () => {
+  try {
+    data.value = await get('/api/OpenData'); // 假設你後端有這個
+  } catch (e) {
+    error.value = e.message;
+  }
+});
+</script>
+
+<template>
+  <div>
+    <h1>WWM Open Data</h1>
+
+    <div v-if="error" style="color:red">
+      {{ error }}
+    </div>
+
+    <pre v-if="data">
+      {{ data }}
+    </pre>
+  </div>
+</template>
